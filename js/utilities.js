ecc0daf4e11bd82691617a5fd20ad06cb1abdf51
khalid199701 <ayon201401@gmail.com>
2024-09-23 18:29:32 +0600

    ui almost done

--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,8 @@
-console.log("utilities");
+function getInputValue(id){
+    const input = parseFloat(document.getElementById(id).value);
+    return input;
+}
+function getText(id){
+    const txt = parseFloat(document.getElementById(id));
+    return txt;
+}
